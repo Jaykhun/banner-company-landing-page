59e69fe094d76ff435644d34363cedb819d5c5e9
Jaykhun Dosibaev <85105007+JAYKHUN@users.noreply.github.com>
2021-10-25 12:12:19 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.feedbck-slider').slick({
+    infinite: true,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    arrows: true,
+    nextArrow: '.feedback-right',
+    prevArrow: '.feedback-left',
+    dots: false,
+});
+
